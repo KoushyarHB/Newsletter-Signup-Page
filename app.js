6799ae917523895b04c45503679917df33b5109d
Koushyar <koushyarheidaribateni@gmail.com>
2023-08-03 20:34:00 +0330

    Port from environment variable

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the "Public" directory
 app.use(express.static("Public"));
